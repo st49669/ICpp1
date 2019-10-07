385cb179644adfaad6e849ff1fddf0287c01271e
st49669 <44142257+st49669@users.noreply.github.com>
2019-10-07 19:13:04 +0200

    Update Main.cpp

--- Session03_Work/Main.cpp
+++ Session03_Work/Main.cpp
@@ -1,4 +1,4 @@
-// main.cpp : Defines the entry point for the console application.
+// Main.cpp
 //
 
 #include "stdafx.h"
@@ -11,8 +11,8 @@ using namespace std;
 
 int main()
 {
-	Pokladna* pokl = new Pokladna;
-	
+	Pokladna * pokl =  new Pokladna;
+
 	pokl->vystavUctenku(12.05, 24);
 	pokl->vystavUctenku(31.99, 20);
 	pokl->vystavUctenku(109.99, 15);
@@ -20,23 +20,23 @@ int main()
 
 	Uctenka* u = new Uctenka[4];
 
-	u[0] = pokl->dejUctenku(1000);
-	u[1] = pokl->dejUctenku(1001);
-	u[2] = pokl->dejUctenku(1002);
-	u[3] = pokl->dejUctenku(1003);
+	u[0] = pokl->dejUctenku(500);
+	u[1] = pokl->dejUctenku(501);
+	u[2] = pokl->dejUctenku(502);
+	u[3] = pokl->dejUctenku(503);
 
-	cout << "Kontrola naplneni uctenek: CisloUctenky;CastkaBezDph;Dph" << endl;
+	cout << "Kontrola naplneni uctenek: CisloUctenky ; CastkaBezDph ; Dph" << endl;
 
 	for (int i = 0; i < 4; i++) {
-		cout << "Uctenka "<<i+1<<".: "<< u[i].getCislo() << ";" << u[i].getCastka() << ";" << u[i].getDph() << endl;
+		cout << "Uctenka "<<i+1<<".: "<< u[i].GetCisloUctenky() << " ; " << u[i].GetCastka() << " ; " << u[i].GetDph() << endl;
 	}
 	
 	cout << endl << "Celkova castka bez DPH: " << pokl->dejCastku() << endl;
 	cout << "Celkova castka s DPH: " << pokl->dejCastkuVcDph() << endl;
 
+
 	delete[] u;
 	delete pokl;
-	
 	system("pause");
     return 0;
 }
