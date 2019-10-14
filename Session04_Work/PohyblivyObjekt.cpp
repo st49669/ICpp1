31dcdd030183fb354f853f15792439414ecf010c
st49669 <44142257+st49669@users.noreply.github.com>
2019-10-14 19:30:07 +0200

    Create PohyblivyObjekt.cpp

---
+++ Session04_Work/PohyblivyObjekt.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "PohyblivyObjekt.h"
+
+
+
+
+double PohyblivyObjekt::GetUhelNatoceni() const
+{
+	return 0.0;
+}
+
+void PohyblivyObjekt::SetUhelNatoceni(double uhelNatoceni) 
+{
+	this->uhelNatoceni = uhelNatoceni;
+}
