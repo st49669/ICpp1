d8d882323e8b15a7b83db8eaaf763e987e572843
st49669 <44142257+st49669@users.noreply.github.com>
2019-10-14 19:54:08 +0200

    Update Hra.cpp

--- Session04_Work/Hra.cpp
+++ Session04_Work/Hra.cpp
@@ -6,13 +6,40 @@
 
 void Hra::VlozObjekt(Objekt * o)
 {
-	//TODO: impelentace
+	if (pocetVytvorenychObjektu >= 100) {
+		throw pocetVytvorenychObjektu;
+	}
+	else {
+		this->objekty[pocetVytvorenychObjektu] = o;
+		pocetVytvorenychObjektu++;
+	}
+	
 }
 
 int * Hra::NajdiIdStatickychObjektu(double xmin, double xmaxm, double ymin, double ymax)
 {
-	//TODO: impelentace
+	int pocetStatObj = 0;
+	for (int i = 0; i < pocetVytvorenychObjektu; i++) {
+		if (StatickyObjekt* t = dynamic_cast<StatickyObjekt*>(this->objekty[i])) {
+			pocetStatObj++;
+		}
+	}
+
+	
+	if (pocetStatObj > 0) {
+		int * vsechnaId = new int[pocetStatObj];
+		int pozicePoleVsechnaId = 0;
+		for (int i = 0; i < pocetVytvorenychObjektu; i++) {
+			if (StatickyObjekt* t = dynamic_cast<StatickyObjekt*>(this->objekty[i])) {
+				vsechnaId[pozicePoleVsechnaId] = this->objekty[i]->getID();
+			}
+		}
+		return vsechnaId;
+	}
+
+	
 	return nullptr;
+	
 }
 
 PohyblivyObjekt ** Hra::NajdiPohybliveObjektyVOblasti(double x, double y, double r)
