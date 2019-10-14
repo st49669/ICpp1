8c7ce508c4687a00884d088fff8e2abb028d83b2
st49669 <44142257+st49669@users.noreply.github.com>
2019-10-14 19:27:39 +0200

    Create Monstrum.cpp

---
+++ Session04_Work/Monstrum.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "Monstrum.h"
+
+int Monstrum::GetHp() const
+{
+	return 0;
+}
+
+void Monstrum::SetHP(int hp)
+{
+	this->hp = hp;
+}
+
+
+int Monstrum::GetMaxHP() const
+{
+	return 0;
+}
+
+void Monstrum::SetMaxHP(int maxhp) 
+{
+	this->maxhp = maxhp;
+}
+
+
