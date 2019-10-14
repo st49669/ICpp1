734ba7f4fe4b46fb4ae1f5f5b2b898e02ade5861
st49669 <44142257+st49669@users.noreply.github.com>
2019-10-14 19:28:03 +0200

    Create Hra.cpp

---
+++ Session04_Work/Hra.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "Hra.h"
+
+
+
+
+void Hra::VlozObjekt(Objekt * o)
+{
+	//TODO: impelentace
+}
+
+int * Hra::NajdiIdStatickychObjektu(double xmin, double xmaxm, double ymin, double ymax)
+{
+	//TODO: impelentace
+	return nullptr;
+}
+
+PohyblivyObjekt ** Hra::NajdiPohybliveObjektyVOblasti(double x, double y, double r)
+{
+	//TODO: impelentace
+	return nullptr;
+}
+
+PohyblivyObjekt ** Hra::NajdiPohybliveObjektyVOblasti(double x, double y, double r, double umin, double umax)
+{
+	//TODO: impelentace
+	return nullptr;
+}
